f889f4c9a2fc0ea4de47a13bd66653e1476fdd30
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-12 20:44:21 +0300

    User model

---
+++ models/User.js
@@ -0,0 +1,68 @@
+'use strict';
+const isAfter = require('date-fns/isAfter');
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    firstName: {
+      field: 'first_name',
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    lastName: {
+      field: 'last_name',
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    birthday: {
+      type: DataTypes.DATEONLY,
+      validate: {
+        isDate: true,
+        isValidDate(value) {
+          if(isAfter(new Date(value), new Date())) { // дата, яку ми прокидуємо в стовпець після поточної
+            throw new Error('Your birtday must be earlier than today');
+          }
+        } 
+      }
+    },
+    gender: {
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    underscored: true
+  });
+  return User;
+};
