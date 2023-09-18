63099d4d2d1a60546cd44d150a16ec3b4ee2564e
Roman Ratushnyi <freshcode1001@ukr.net>
2023-09-18 20:23:00 +0300

    Refactor router

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,13 @@
 const { Router } = require('express');
-const UserController = require('../controllers/User.controller');
-const TaskController = require('../controllers/Task.controller');
-const GroupController = require('../controllers/Group.controller');
-const { getUserInstance, validateUser } = require('../middlewares/user.mv');
-const { validateTask } = require('../middlewares/task.mw');
+const userRouter = require('./userRouter');
+const taskRouter = require('./taskRouter');
+const groupRouter = require('./groupRouter');
 
 const router = Router();
 
-// user section
-router.post('/user', validateUser, UserController.createUser);
-router.get('/users', UserController.findAll);
-router.get('/user/:userId', getUserInstance, UserController.findOneById);
-router.get('/users/groups/:userId', UserController.getUserWithGroups);
-router.delete('/user/:userId', UserController.deleteById);
-router.put('/user/:userId', getUserInstance, UserController.updateById);
+router.use('/users', userRouter);
+router.use('/task', taskRouter);
+router.use('/groups', groupRouter);
 
-// task section
-router.post('/task/:userId', validateTask, getUserInstance, TaskController.createTask);
-router.get('/tasks/:userId', getUserInstance, TaskController.getAllUserTasks);
-router.get('/task-count/:userId', getUserInstance, TaskController.getCountOfTasks);
 
-// group section
-router.post('/groups', GroupController.createGroup);
-router.put('/groups/:userId/:groupId', getUserInstance, GroupController.addUserToGroup)
-router.get('/groups/:userId', getUserInstance, GroupController.getUserGroups);
-router.delete('/groups/:userId/:groupId', getUserInstance, GroupController.deleteUserFromGroup);
 module.exports = router;
---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const UserController = require('../controllers/User.controller');
+const { getUserInstance, validateUser } = require('../middlewares/user.mv');
+
+const userRouter = Router();
+
+// user section
+userRouter.post('/', validateUser, UserController.createUser);
+userRouter.get('/', UserController.findAll);
+userRouter.get('/:userId', getUserInstance, UserController.findOneById);
+userRouter.get('/groups/:userId', UserController.getUserWithGroups);
+userRouter.delete('/:userId', UserController.deleteById);
+userRouter.put('/:userId', getUserInstance, UserController.updateById);
+
+module.exports = userRouter;
